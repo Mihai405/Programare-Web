97e56e0c96e447c113d9ac4284af2b5f3cb981f7
Mihai Jurj <mihai.jurj@nagarro.com>
2021-10-09 18:05:01 +0300

    Register

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import  Nav  from './Nav';
+import { Register } from './Register';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App(){
+    return (
+        <BrowserRouter>
+            <Nav />
+            <Switch>
+                <Route exact path='/' component={ () =><h1>HomePage</h1>} />
+                <Route path='/login' component={ () => <h1>Login</h1>} />
+                <Route path='/register' component={Register} />
+            </Switch>
+        </BrowserRouter>
+    );
 }
-
-export default App;
---
+++ src/Register.js
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
+import { Grid, Paper, Typography } from "@material-ui/core";
+import "./Register.css";
+
+export function Register() {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    retypePassword: "",
+  });
+
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+    retypePassword: "",
+    passwordMatch: "",
+  });
+
+  const handleInputChange = (event) => {
+    const newErrors = { ...errors };
+    if (
+      event.target.id === "password" ||
+      event.target.id === "retypePassword"
+    ) {
+      newErrors.passwordMatch = "";
+    }
+    newErrors[event.target.id] = "";
+    setErrors(newErrors);
+    setValues({ ...values, [event.target.id]: event.target.value });
+  };
+
+  const formIsValid = () => {
+    const newErrors = { ...errors };
+    let isValid = true;
+    const validEmail =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!validEmail.test(String(values.email).toLowerCase())) {
+      newErrors.email = "Email is not valid";
+      isValid = false;
+    }
+
+    if (values.password.length < 5) {
+      newErrors.password = "Password is not valid";
+      isValid = false;
+    }
+
+    if (values.retypePassword === "") {
+      newErrors.retypePassword = "RetypePassword is required";
+      isValid = false;
+    }
+
+    if (values.password !== values.retypePassword) {
+      newErrors.passwordMatch = "Password didn't match";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid()) return;
+
+    const { retypePassword, ...safeValues } = values;
+    console.log(safeValues);
+  };
+
+  return (
+    <Grid>
+      <Paper elevation={20} className="paper">
+        <Grid align="center">
+          <h1 id="headerText">Register</h1>
+          <Typography variant="subtitle1" gutterBottom>
+            Please fill this form to create an account !
+          </Typography>
+        </Grid>
+        <form onSubmit={handleSubmit} noValidate>
+          <div>
+            <TextField
+              fullWidth
+              error={errors.email !== "" ? true : false}
+              type="email"
+              id="email"
+              label="Email"
+              onChange={handleInputChange}
+              value={values.email}
+              helperText={errors.email}
+            />
+          </div>
+          <div>
+            <TextField
+              fullWidth
+              error={errors.password !== "" ? true : false}
+              type="password"
+              id="password"
+              label="Password"
+              onChange={handleInputChange}
+              value={values.password}
+              helperText={errors.password}
+            />
+          </div>
+          <div>
+            <TextField
+              fullWidth
+              error={errors.retypePassword !== "" ? true : false}
+              type="password"
+              id="retypePassword"
+              label="Retype Password"
+              onChange={handleInputChange}
+              value={values.retypePassword}
+              helperText={errors.retypePassword}
+            />
+          </div>
+          <div>
+            {errors.passwordMatch !== "" ? (
+              <Alert severity="error">{errors.passwordMatch}!</Alert>
+            ) : null}
+          </div>
+          <div align="center">
+            <Button variant="contained" type="submit">
+              Register
+            </Button>
+          </div>
+        </form>
+      </Paper>
+    </Grid>
+  );
+}
